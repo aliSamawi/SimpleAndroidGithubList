25dfcae2f5056ad464fae4a4a88f6b833af6b099
ali samawi <ali@alis-MacBook-Pro.local>
2018-08-17 17:09:16 +0430

    init repository

--- app/src/androidTest/java/github/github/githubtrend/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/github/github/githubtrend/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package github.com.githubtrend
+package github.github.githubtrend
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
---
+++ app/src/main/java/com/github/githubtrend/injection/module/AppContextModule.kt
@@ -0,0 +1,18 @@
+package com.github.githubtrend.injection.module
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+
+/**
+ * module to represent context for all needed provides
+ *
+ * Created by ali on 8/17/2018 AD.
+ */
+
+@Module
+class AppContextModule(val context: Context) {
+
+    @Provides
+    fun provideContext(): Context = context
+}
